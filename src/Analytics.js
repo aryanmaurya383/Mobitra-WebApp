2ca2e72045407b1781e0aed31854f477d97161f6
Aryan M <aryanms21@iitk.ac.in>
2022-12-10 16:37:07 +0530

    header created

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './Analytics.css';
 
 function Analytics() {
     return (
-        <div>Analytics</div>
+        <div className='analytics'>
+            <div className='analytics__leftBody'>
+                <div className='analytics__leftBody__header'>
+                    <div className='analytics__leftBody__header__content'>Yesterday</div>
+                    <div className='analytics__leftBody__header__content'>7 Day</div>
+                    <div className='analytics__leftBody__header__content'>15 Day</div>
+                    <div className='analytics__leftBody__header__content'>30 Day</div>
+                    <div className='analytics__leftBody__header__content'>icon 1 Jan 2022- 2 Feb 2022</div>
+                    <select className='analytics__leftBody__header__content flex-right' name="cars" id="cars">
+                        <option value="volvo">Distance</option>
+                        <option value="saab">Fuel</option>
+                        <option value="mercedes">Time</option>
+                    </select>
+
+                </div>
+                <div className="analytics__leftBody__graph">
+                    GRAPH
+                </div>
+            </div>
+            <div className='analytics__rightBody'>
+                <div><h1>Vehicles</h1></div>
+            </div>
+
+        </div>
     )
 }
 
