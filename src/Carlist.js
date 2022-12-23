e427d1b4358998952307dba4443e7b6e44983427
the-7g-girl <sharvaniuj21@iitk.ac.in>
2022-12-23 19:06:22 +0530

    maps updated

---
+++ src/Carlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Carlist.css';
+
+export default function Carlist() {
+    return (
+        <div className='cars-list'>
+                    <h5>All</h5> 
+                    <h5>Running</h5>
+                    <h5>Idle</h5>
+                    <h5>Parked</h5>
+                
+        </div>
+    )
+}
